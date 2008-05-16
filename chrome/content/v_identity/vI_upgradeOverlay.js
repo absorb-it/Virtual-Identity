ac3969ae8f29053cebaf12e5d05f077c86690126
rene <rene@b0298737-1536-0410-8877-d4856560ed59>
2008-05-16 17:16:25 +0000

    changed back to open as a Dialog.

--- chrome/content/v_identity/vI_upgradeOverlay.js
+++ chrome/content/v_identity/vI_upgradeOverlay.js
@@ -26,8 +26,8 @@ vI_upgradeOverlay = {
 	init: function() {
 		vI_rdfDatasource.init() // just to be sure that Datasource is available
 		if (vI_rdfDatasource.rdfUpgradeRequired() || vI_rdfDatasource.extUpgradeRequired())		
-			window.open("chrome://v_identity/content/vI_upgrade.xul",0, // give the Dialog a unique id
-				"chrome, modal, alwaysRaised, resizable=yes").focus();
+			window.openDialog("chrome://v_identity/content/vI_upgrade.xul",0, // give the Dialog a unique id
+				"chrome, dialog, modal, alwaysRaised, resizable=yes").focus();
 		else vI_account.cleanupSystem(); // always clean leftover accounts and directories
 	}
 }
