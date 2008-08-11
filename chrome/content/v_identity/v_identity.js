b0521512e04a842b29afdfeef27039f9b1dbcd9f
rene <rene@b0298737-1536-0410-8877-d4856560ed59>
2008-08-11 15:51:34 +0000

    added code for statusmenu

--- chrome/content/v_identity/v_identity.js
+++ chrome/content/v_identity/v_identity.js
@@ -22,6 +22,118 @@
     Contributor(s):
  * ***** END LICENSE BLOCK ***** */
 
+var vI_statusmenu = {
+	prefroot : Components.classes["@mozilla.org/preferences-service;1"]
+		.getService(Components.interfaces.nsIPrefService)
+		.getBranch(null),
+
+	objStatusMenu : null,
+	objSaveBaseIDMenuItem : null,
+	objStorageSaveMenuItem : null,
+	objStatusMenuSeparator : null,
+	objSaveSwitch : null,
+	objStatusText : null,
+	
+	observe: function(subject, topic, data) {
+		switch (data) {
+			case "extensions.virtualIdentity.storage_show_switch":
+				vI_statusmenu.objSaveSwitch.setAttribute("hidden", !vI_statusmenu.prefroot.getBoolPref(data));
+				// no break, continue like with storedefault
+			case "extensions.virtualIdentity.storage_storedefault":
+				vI_statusmenu.objStorageSaveMenuItem.setAttribute("checked", vI_statusmenu.prefroot.getBoolPref("extensions.virtualIdentity.storage_storedefault"));
+				break;
+			case "extensions.virtualIdentity.storage_store_base_id":
+				vI_statusmenu.objSaveBaseIDMenuItem.setAttribute("checked", vI_statusmenu.prefroot.getBoolPref(data));
+				break;
+			case "extensions.virtualIdentity.storage":
+				if (vI_statusmenu.prefroot.getBoolPref(data)) {
+					vI_statusmenu.objStorageSaveMenuItem.removeAttribute("hidden");
+					vI_statusmenu.objSaveBaseIDMenuItem.removeAttribute("hidden");
+					vI_statusmenu.objStatusMenuSeparator.removeAttribute("hidden");
+				}
+				else {
+					vI_statusmenu.objStorageSaveMenuItem.setAttribute("hidden", "true");
+					vI_statusmenu.objSaveBaseIDMenuItem.setAttribute("hidden", "true");
+					vI_statusmenu.objStatusMenuSeparator.setAttribute("hidden", "true");
+				}
+				break;
+		}
+		vI_statusmenu.menuConstraint(vI_statusmenu.objStorageSaveMenuItem);
+	},
+	
+	addObserver: function() {
+		vI_statusmenu.prefroot.addObserver("extensions.virtualIdentity.storage", vI_statusmenu, false);
+		vI_statusmenu.prefroot.addObserver("extensions.virtualIdentity.storage_show_switch", vI_statusmenu, false);
+		vI_statusmenu.prefroot.addObserver("extensions.virtualIdentity.storage_storedefault", vI_statusmenu, false);	
+		vI_statusmenu.prefroot.addObserver("extensions.virtualIdentity.storage_store_base_id", vI_statusmenu, false);	
+	},
+	
+	removeObserver: function() {
+		vI_statusmenu.prefroot.removeObserver("extensions.virtualIdentity.storage", vI_statusmenu);
+		vI_statusmenu.prefroot.removeObserver("extensions.virtualIdentity.storage_show_switch", vI_statusmenu);
+		vI_statusmenu.prefroot.removeObserver("extensions.virtualIdentity.storage_storedefault", vI_statusmenu);
+		vI_statusmenu.prefroot.removeObserver("extensions.virtualIdentity.storage_store_base_id", vI_statusmenu);
+	},
+	
+	init : function () {
+		vI_statusmenu.prefroot.QueryInterface(Components.interfaces.nsIPrefBranch2);
+
+		vI_statusmenu.objStatusMenu = document.getElementById("vI-status-menu");
+		vI_statusmenu.objSaveBaseIDMenuItem = document.getElementById("vI_statusMenu_storage_saveBaseID");
+		vI_statusmenu.objStorageSaveMenuItem = document.getElementById("vI_statusMenu_storage_save");
+		vI_statusmenu.objStatusMenuSeparator = document.getElementById("vI_statusMenu_separator");
+		vI_statusmenu.objSaveSwitch = document.getElementById("storage_save");
+		vI_statusmenu.objStatusText = document.getElementById("statusText");
+
+		vI_statusmenu.addObserver();
+		vI_statusmenu.observe(null, null, "extensions.virtualIdentity.storage_show_switch");
+		vI_statusmenu.observe(null, null, "extensions.virtualIdentity.storage_store_base_id");
+		vI_statusmenu.observe(null, null, "extensions.virtualIdentity.storage");
+	},
+	
+	saveBaseIDClicked : function () {
+		vI_statusmenu.menuConstraint(vI_statusmenu.objStorageSaveMenuItem);
+	},
+
+	__timeout : 5,	// timeout for status messages in seconds
+	__addStatusMessage : function(save) {
+		var message = vI.elements.strings.getString("vident.statusText.save." + save);
+		vI_statusmenu.objStatusText.setAttribute("label", message);
+		window.setTimeout(vI_statusmenu.__clearStatusMessage, vI_statusmenu.__timeout * 1000);
+	},
+
+	__clearStatusMessage : function() {
+		vI_statusmenu.objStatusText.setAttribute("label", "");
+	},
+
+	menuConstraint : function (elem) {
+		var save = "off";
+		if (vI_statusmenu.prefroot.getBoolPref("extensions.virtualIdentity.storage") &&
+			elem.getAttribute("checked") == "true") {
+			if (vI_statusmenu.objSaveBaseIDMenuItem.getAttribute("checked") == "true")
+				save = "base";
+			else save = "ok";
+		}
+		vI_statusmenu.objStatusMenu.setAttribute("save", save);
+		vI_statusmenu.__addStatusMessage(save);
+	},
+
+	clicked : function (button) {
+		if (button != 0) return; // only react on left mouse button
+		if (!vI_statusmenu.prefroot.getBoolPref("extensions.virtualIdentity.storage")) return;
+
+		var curSaveStatus = (vI_statusmenu.objStorageSaveMenuItem.getAttribute("checked") == "true");
+		var curSaveBaseIDStatus = (vI_statusmenu.objSaveBaseIDMenuItem.getAttribute("checked") == "true");
+		var newSaveStatus = ((!curSaveStatus) || (curSaveStatus && !curSaveBaseIDStatus))
+		var newSaveBaseIDStatus = (!curSaveBaseIDStatus && curSaveStatus)
+		vI_statusmenu.objStorageSaveMenuItem.setAttribute("checked", newSaveStatus)
+		vI_statusmenu.objSaveBaseIDMenuItem.setAttribute("checked", newSaveBaseIDStatus)
+
+		var elem = vI_statusmenu.objStorageSaveMenuItem;
+		vI_statusmenu.menuConstraint(elem);
+	},
+}
+
 var vI = {
 	preferences : Components.classes["@mozilla.org/preferences-service;1"]
 			.getService(Components.interfaces.nsIPrefService)
@@ -213,6 +325,7 @@ var vI = {
 		vI_notificationBar.dump("## v_identity: initSystemStage1.\n")
 		vI_smtpSelector.init();
 		vI_msgIdentityClone.init();
+		vI_statusmenu.init();
 	},
 	
 	initSystemStage2 : function() {
@@ -363,3 +476,6 @@ var vI = {
 vI.replacement_functions.replace_FillIdentityList();
 window.addEventListener('load', vI.init, false);		// TB 1.5x, SM
 window.addEventListener('compose-window-init', vI.init, true);	// TB 2.x 3.x
+
+window.addEventListener("unload", function(e) { try {vI_statusmenu.removeObserver();} catch (ex) { } }, false);
+
