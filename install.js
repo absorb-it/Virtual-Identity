7bd8ffe32010b6dfac76cb93169b5356c3cecf81
rene <rene@b0298737-1536-0410-8877-d4856560ed59>
2008-08-13 13:58:41 +0000

    added polish locales

--- install.js
+++ install.js
@@ -21,7 +21,7 @@ var skinDir           = "/" + name + "/"
 //var localeDir           = "/"
 var localeDir           = "/" + name + "/"
 
-var locales             = new Array( "en-US", "de-DE", "fr-FR", "nl-NL", "cs-CZ", "it-IT", "es-ES" );
+var locales             = new Array( "en-US", "de-DE", "fr-FR", "nl-NL", "cs-CZ", "it-IT", "es-ES", "pl-PL" );
 var skins               = new Array( "classic" ); // "modern"
 var prefs               = new Array( "preferences.js" );
 var components          = new Array(  );
