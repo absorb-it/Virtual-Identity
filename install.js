3e173fed0a7325e10e0da96168c1b6753cdeb486
rene <rene@b0298737-1536-0410-8877-d4856560ed59>
2008-08-21 16:00:21 +0000

    tagged as 0.5.6pre4

--- install.js
+++ install.js
@@ -6,7 +6,7 @@
 
 // Editable Items Begin
 var displayName         = "Virtual Identity"; // The name displayed to the user (don't include the version)
-var version             = "0.5.6pre3";
+var version             = "0.5.6pre4";
 var name                = "v_identity"; // The leafname of the JAR file (without the .jar part)
 
 // The following three sets of variables tell this installer script how your
