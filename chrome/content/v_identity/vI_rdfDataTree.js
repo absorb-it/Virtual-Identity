44a21d2aef3190069ae68030f9e61fc3bd41bdb5
rene <rene@b0298737-1536-0410-8877-d4856560ed59>
2009-01-13 00:02:23 +0000

    tabbed rdf Data Editor (not completely working)

--- chrome/content/v_identity/vI_rdfDataTree.js
+++ chrome/content/v_identity/vI_rdfDataTree.js
@@ -28,115 +28,158 @@
     http://www.ibiblio.org/pub/Linux/LICENSES/mit.license
 */
 
-var vI_rdfDataTree = {
-	__idTable : null,
-	__idData : null,
-	__treeElem : null,
-	filterText : "",
+
+//prepares an object for easy comparison against another. for strings, lowercases them
+function prepareForComparison (o) {
+	if (typeof o == "string") { return o.toLowerCase().replace(/\"/g,""); }
+	return "";
+};
+
+
+function rdfDataTree(sourceName) {
+	this.treeElem = document.getElementById("rdfDataTree_" + sourceName);
+	this.filterText = "";
+}
+rdfDataTree.prototype = {
+	idTable : null,
+	idData : null,
+	treeElem : null,
+	filterText : null,
 	
-	__strings : null,
-
-	promptService : Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-			.getService(Components.interfaces.nsIPromptService),
-
-	init : function() {
-		vI_rdfDatasource.init();
-		vI_rdfDataTree.__treeElem = document.getElementById("threadTree");
-		vI_rdfDataTree.__strings = document.getElementById("vI_rdfDataTreeBundle");
-		vI_storageExtrasHelper.hideUnusedTreeCols();
-		vI_rdfDataTree.loadTable();
+	//this function is called every time the tree is sorted, filtered, or reloaded
+	loadTable : function(container) {
+		//remember scroll position. this is useful if this is an editable table
+		//to prevent the user from losing the row they edited
+		var topVisibleRow = null;
+		if (this.idTable) { topVisibleRow = this.treeElem.treeBoxObject.getFirstVisibleRow(); }
+		if (this.idData == null) {
+			this.idData = [];
+			vI_rdfDatasource.readAllEntriesFromRDF(container, this);
+		}
+		if (this.filterText == "") {
+			//show all of them
+			this.idTable = this.idData;
+		} else {
+			//filter out the ones we want to display
+			this.idTable = [];
+			this.idData.forEach(function(element) {
+				//we'll match on every property
+				for (var i in element) {
+					if (prepareForComparison(element[i]).indexOf(this.filterText) != -1) {
+						this.idTable.push(element);
+						break;
+					}
+				}
+			});
+		}	
+		this.sort();
+		//restore scroll position
+		if (topVisibleRow) {
+			this.treeElem.treeBoxObject.scrollToRow(topVisibleRow);
+		}
 	},
 
-	//prepares an object for easy comparison against another. for strings, lowercases them
-	__prepareForComparison : function(o) {
-		if (typeof o == "string") { return o.toLowerCase().replace(/\"/g,""); }
-		return "";
-	},
-	
-	__addNewDatum : function(resource, type, name, localIdentityData) {
+	addNewDatum : function(resource, name, localIdentityData, idData) {
 		var pref = { 	recipientCol : name,
-				typeCol : document.getElementById("vI_rdfDataTreeBundle").getString("vI_rdfDataTree.dataType." + type),
+				typeCol : "type",
 				senderCol : localIdentityData.combinedName,
 				smtpCol : localIdentityData.smtp.value,
 				smtpKey : localIdentityData.smtp.key,
 				idCol : localIdentityData.id.value,
 				idKey : localIdentityData.id.key,
 				resource : resource,
-				type : type,
-				identityData : localIdentityData }
-		
+				identityData : localIdentityData}
+// 		vI_notificationBar.dump("## addNewDatum.\n");
 		localIdentityData.extras.addPrefs(pref);
-		vI_rdfDataTree.__idData.push(pref);
+		idData.push(pref);
 	},
-	
-	//this function is called every time the tree is sorted, filtered, or reloaded
-	loadTable : function() {
-		//remember scroll position. this is useful if this is an editable table
-		//to prevent the user from losing the row they edited
-		var topVisibleRow = null;
-		if (vI_rdfDataTree.__idTable) {
-			topVisibleRow = vI_rdfDataTree.__treeElem.treeBoxObject.getFirstVisibleRow();
-		}
-		if (vI_rdfDataTree.__idData == null) {
-			vI_rdfDataTree.__idData = [];
-			vI_rdfDatasource.readAllVIdentitiesFromRDF(vI_rdfDataTree.__addNewDatum)
-		}
-		if (vI_rdfDataTree.filterText == "") {
-			//show all of them
-			vI_rdfDataTree.__idTable = vI_rdfDataTree.__idData;
-		} else {
-			//filter out the ones we want to display
-			vI_rdfDataTree.__idTable = [];
-			vI_rdfDataTree.__idData.forEach(function(element) {
-				//we'll match on every property
-				for (var i in element) {
-					if (vI_rdfDataTree.__prepareForComparison(element[i]).indexOf(vI_rdfDataTree.filterText) != -1) {
-						vI_rdfDataTree.__idTable.push(element);
-						break;
-					}
-				}
-			});
-		}
-		vI_rdfDataTree.sort();
-		//restore scroll position
-		if (topVisibleRow) {
-			vI_rdfDataTree.__treeElem.treeBoxObject.scrollToRow(topVisibleRow);
-		}
-	},
-	
 	sort : function(column) {
 		var columnName;
-		var order = vI_rdfDataTree.__treeElem.getAttribute("sortDirection") == "ascending" ? 1 : -1;
+		var order = this.treeElem.getAttribute("sortDirection") == "ascending" ? 1 : -1;
 		//if the column is passed and it's already sorted by that column, reverse sort
 		if (column) {
 			columnName = column.id;
-			if (vI_rdfDataTree.__treeElem.getAttribute("sortResource") == columnName) {
+			if (this.treeElem.getAttribute("sortResource") == columnName) {
 				order *= -1;
 			}
 		} else {
-			columnName = vI_rdfDataTree.__treeElem.getAttribute("sortResource");
+			columnName = this.treeElem.getAttribute("sortResource");
 		}
 		
 		function columnSort(a, b) {
-			if (vI_rdfDataTree.__prepareForComparison(a[columnName]) > 
-				vI_rdfDataTree.__prepareForComparison(b[columnName])) return 1 * order;
-			if (vI_rdfDataTree.__prepareForComparison(a[columnName]) < 
-				vI_rdfDataTree.__prepareForComparison(b[columnName])) return -1 * order;
+			if (prepareForComparison(a[columnName]) > 
+				prepareForComparison(b[columnName])) return 1 * order;
+			if (prepareForComparison(a[columnName]) < 
+				prepareForComparison(b[columnName])) return -1 * order;
 			return 0;
 		}
-		vI_rdfDataTree.__idTable.sort(columnSort);
+		this.idTable.sort(columnSort);
 		//setting these will make the sort option persist
-		vI_rdfDataTree.__treeElem.setAttribute("sortDirection", order == 1 ? "ascending" : "descending");
-		vI_rdfDataTree.__treeElem.setAttribute("sortResource", columnName);
-		vI_rdfDataTree.__treeElem.view = new vI_rdfDataTree.treeView(vI_rdfDataTree.__idTable);
+		this.treeElem.setAttribute("sortDirection", order == 1 ? "ascending" : "descending");
+		this.treeElem.setAttribute("sortResource", columnName);
+		this.treeElem.view = new vI_rdfDataTree.treeView(this.idTable);
 		//set the appropriate attributes to show to indicator
-		var cols = vI_rdfDataTree.__treeElem.getElementsByTagName("treecol");
+		var cols = this.treeElem.getElementsByTagName("treecol");
 		for (var i = 0; i < cols.length; i++) {
 			cols[i].removeAttribute("sortDirection");
 		}
 		document.getElementById(columnName).setAttribute("sortDirection", order == 1 ? "ascending" : "descending");
 	},
 
+}
+
+var vI_rdfDataTree = {
+	promptService : Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+			.getService(Components.interfaces.nsIPromptService),
+
+	trees : {},
+	tabbox : null,
+	
+	_strings : null,
+	
+	onselect : function () {
+vI_notificationBar.dump("## onselect " + vI_rdfDataTree.tabbox.selectedPanel.id + ".\n");
+		var tree = vI_rdfDataTree.trees[vI_rdfDataTree.tabbox.selectedPanel.id];
+		var htmlBox = document.getElementById("vI_rdfDataTreeInfoBox")
+		if (tree.treeElem.view.selection.count != 1)
+			{ vI_rdfDataTree.hideInfoBox(); return; }
+		
+		var identityData = tree.idTable[tree.treeElem.currentIndex]["identityData"];
+		var _identityInfo = 
+			"<div id='recipientLabel'>" +
+				tree.idTable[tree.treeElem.currentIndex]["recipientCol"].replace(/>/g,"&gt;").replace(/</g,"&lt;") +
+			"</div><div id='vICard'>" +
+			"<table><tr>" +
+				"<td class='image'><img src='chrome://v_identity/skin/vi-info.png' /></td>" +
+				"<td class='identityTable'>" +
+					"<div class='name'>" + identityData.combinedNameHtml + "</div>" +	
+					"<table><tbody>" + identityData.getMatrix() + "</tbody></table>" +
+				"</td>" +
+			"</tr></table></div>"
+
+		htmlBox.outputString = _identityInfo;
+		vI_rdfDataTree.infoBoxHidden = false;
+		htmlBox.setAttribute("style", "height:" + htmlBox.contentDocument.lastChild.scrollHeight +"px");
+		vI_rdfDataTree.overflow(); // better resize one time too much, mozilla is still magic  :)
+	},
+
+	init : function() {
+		vI_rdfDataTree.tabbox = document.getElementById("TreeTabbox");
+		vI_rdfDataTree._strings = document.getElementById("vI_rdfDataTreeBundle");
+
+		vI_rdfDatasource.init();
+		vI_storageExtrasHelper.hideUnusedTreeCols(); // XXX what happens heree ?? :)
+		
+		vI_rdfDataTree.trees["email"] = new rdfDataTree("email");
+		vI_rdfDataTree.trees["email"].loadTable(vI_rdfDatasource.emailContainer);
+		vI_rdfDataTree.trees["maillist"] = new rdfDataTree("maillist");
+		vI_rdfDataTree.trees["maillist"].loadTable(vI_rdfDatasource.maillistContainer);
+		vI_rdfDataTree.trees["newsgroup"] = new rdfDataTree("newsgroup");
+		vI_rdfDataTree.trees["newsgroup"].loadTable(vI_rdfDatasource.newsgroupContainer);
+		vI_rdfDataTree.trees["filter"] = new rdfDataTree("filter");
+		vI_rdfDataTree.trees["filter"].loadTable(vI_rdfDatasource.filterContainer);
+	},
+	
 	//generic custom tree view stuff
 	treeView : function (table) {
 		this.rowCount = table.length;
@@ -162,8 +205,10 @@ var vI_rdfDataTree = {
 		this.getColumnProperties = function(colid,col,props){};
 		this.cycleHeader = function(col, elem) { vI_rdfDataTree.sort(col) };
 	},
+
 	
 	__setFilter : function (text) {
+// loop trough all trees
 		vI_rdfDataTree.filterText = text;
 		vI_rdfDataTree.loadTable();
 	},
@@ -184,7 +229,8 @@ var vI_rdfDataTree = {
 	},
 	
 	__updateMenu : function(modifySelected, removeSelected) {
-		var noSelections = (vI_rdfDataTree.__treeElem.view.selection.count == 0)
+		var tree = vI_rdfDataTree.trees[vI_rdfDataTree.tabbox.selectedPanel.id];
+		var noSelections = (tree.treeElem.view.selection.count == 0)
 		modifySelected.setAttribute("disabled", noSelections)
 		removeSelected.setAttribute("disabled", noSelections)	
 	},
@@ -202,28 +248,29 @@ var vI_rdfDataTree = {
 	},
 
 	modifySelected : function() {
-		if (vI_rdfDataTree.__treeElem.view.selection.count == 0) return;
-		if (vI_rdfDataTree.__treeElem.view.selection.count > 5) {
-			var warning = vI_rdfDataTree.__strings.getString("vI_rdfDataTree.modify.Warning1") + " " +
-				vI_rdfDataTree.__treeElem.view.selection.count + " " +
-				vI_rdfDataTree.__strings.getString("vI_rdfDataTree.modify.Warning2")
+		var tree = vI_rdfDataTree.trees[vI_rdfDataTree.tabbox.selectedPanel.id];
+		if (tree.treeElem.view.selection.count == 0) return;
+		if (tree.treeElem.view.selection.count > 5) {
+			var warning = vI_rdfDataTree._strings.getString("vI_rdfDataTree.modify.Warning1") + " " +
+				tree.treeElem.view.selection.count + " " +
+				vI_rdfDataTree._strings.getString("vI_rdfDataTree.modify.Warning2")
 			if (!vI_rdfDataTree.promptService.confirm(window,"Warning",warning)) return;
 		}
 		
 		var start = new Object(); var end = new Object();
-		var numRanges = vI_rdfDataTree.__treeElem.view.selection.getRangeCount();
+		var numRanges = tree.treeElem.view.selection.getRangeCount();
 
 		for (var t=0; t<numRanges; t++){
-			vI_rdfDataTree.__treeElem.view.selection.getRangeAt(t,start,end);
+			tree.treeElem.view.selection.getRangeAt(t,start,end);
 			for (var v=start.value; v<=end.value; v++)
 				window.openDialog("chrome://v_identity/content/vI_rdfDataEditor.xul",0,
 					"chrome, dialog, modal, alwaysRaised, resizable=yes",
-					vI_rdfDataTree.__idTable[v],
+					tree.idTable[v],
 					vI_rdfDatasource).focus();
 		}
 		
-		vI_rdfDataTree.__idData = null; vI_rdfDataTree.__idTable = null;
-		vI_rdfDataTree.loadTable();
+		tree.idData = null; tree.idTable = null;
+		vI_rdfDataTree.loadTable(vI_rdfDataTree.tabbox.selectedPanel.id);
 		vI_rdfDataTree.hideInfoBox();
 	},
 	
@@ -251,29 +298,6 @@ var vI_rdfDataTree = {
 		vI_rdfDataTree.hideInfoBox();
 	},
 	
-	onselect : function() {
-		var htmlBox = document.getElementById("vI_rdfDataTreeInfoBox")
-		if (vI_rdfDataTree.__treeElem.view.selection.count != 1)
-			{ vI_rdfDataTree.hideInfoBox(); return; }
-		
-		var identityData = vI_rdfDataTree.__idTable[vI_rdfDataTree.__treeElem.currentIndex]["identityData"];
-		var _identityInfo = 
-			"<div id='recipientLabel'>" +
-				vI_rdfDataTree.__idTable[vI_rdfDataTree.__treeElem.currentIndex]["recipientCol"].replace(/>/g,"&gt;").replace(/</g,"&lt;") +
-			"</div><div id='vICard'>" +
-			"<table><tr>" +
-				"<td class='image'><img src='chrome://v_identity/skin/vi-info.png' /></td>" +
-				"<td class='identityTable'>" +
-					"<div class='name'>" + identityData.combinedNameHtml + "</div>" +	
-					"<table><tbody>" + identityData.getMatrix() + "</tbody></table>" +
-				"</td>" +
-			"</tr></table></div>"
-
-		htmlBox.outputString = _identityInfo;
-		vI_rdfDataTree.infoBoxHidden = false;
-		htmlBox.setAttribute("style", "height:" + htmlBox.contentDocument.lastChild.scrollHeight +"px");
-		vI_rdfDataTree.overflow(); // better resize one time too much :)
-	},
 
 	infoBoxHidden : true,
 	overflow : function() {
