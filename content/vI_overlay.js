c33c1eae4d2b2b8a9b07f55dd335dc04ebdc38d5
rene <rene@absorb.it>
2011-11-22 11:52:36 +0100

    removed threadTreeColumns and created seperate extension

--- content/vI_overlay.js
+++ content/vI_overlay.js
@@ -4,7 +4,6 @@ virtualIdentityExtension.ns(function() { with (virtualIdentityExtension.LIB) {
   // this is the entry place, nameSpaceWrapper is loaded and the show can start
   try {
     Components.utils.import("resource://v_identity/plugins/conversations.js", virtualIdentityExtension);
-    Components.utils.import("resource://v_identity/vI_threadTreeColumns.js", virtualIdentityExtension);
   } catch(e) {
     vI.dumpCallStack(e);
   }
