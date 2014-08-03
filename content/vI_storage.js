c424bd885a995d703c1d04b05c003339ef8e36e5
rene <rene@absorb.it>
2014-08-03 11:29:14 +0200

    still not robust enough?

--- content/vI_storage.js
+++ content/vI_storage.js
@@ -72,11 +72,11 @@ var storage = {
 		// only react on events triggered by addressCol2 - textinput Elements
 		if (!element || ! element.id.match(/^addressCol2*/)) return;
 		Log.debug("awOnBlur '" + element.id + "' '" + element.value  + "'");
-        if (element.value == "") {
+        if (element.value == "" || typeof element.value == 'undefined') {
             element.value = element.getAttribute("value");
             Log.debug("awOnBlur second try'" + element.id + "' '" + element.value  + "'");
         }
-        if (element.value == "") return;
+        if (element.value == "" || typeof element.value == 'undefined') return;
         storage.__updateVIdentityFromStorage(element, currentWindow);
 		storage.focusedElement = null;
 	},
